fd72b7ce9a7c4d2d521a149e12cfebbb20cddc76
Jonathan C <jcc064@lionmail.lindenwood.edu>
2017-04-21 12:37:14 -0500

    Current Demo

---
+++ app.js
@@ -0,0 +1,11 @@
+function greeting() {
+    alert("Hello, welcome to my page!");
+}
+
+window.onload = greeting;
+//code functionality that makes a click event that will randomize videos for the showcase of artists and their work
+//to further detail the functionality:
+    //make it so each click will randomly produce a video (along with the correct artist name above said video)
+    //I'd assume Angular would be proper for this kind of functionality. Each of the 3 or 6 sub sections //will contain an array of videos that can be called (maybe randomly, probably not for practicality)
+//use angular to call an array for the carousel content
+//make sure all the content is stored on the machine, aside from the youtube videos. It'll probably make load quicker.
